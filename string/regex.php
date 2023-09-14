3cf9d76f62ea80c949e28c011a12379f0c96820c
unknown <lucas.hsilva@icloud.com>
2023-09-14 16:43:20 -0300

    alura

---
+++ string/regex.php
@@ -0,0 +1,26 @@
+<?php 
+
+$telefones = ['(24) 99999 - 9999', '(21) 99999 - 9999', '(24) 2222 - 2222'];
+
+foreach($telefones as $telefone){
+    $regex = '/\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})/';
+    $telefoneValido = preg_match(
+        $regex,
+        $telefone,
+        $correspondencia
+    );
+
+    var_dump($correspondencia);
+    if($telefoneValido){
+        echo 'Telefone Válido' . PHP_EOL;
+    } else {
+        echo 'Telefone inválido' . PHP_EOL;
+    }
+
+    echo preg_replace(
+        $regex,
+        '(xx) \2',
+        $telefone,        
+    ) . PHP_EOL;
+}
+?>
